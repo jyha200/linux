0ab5b8de3d4dedb17fc8b8424a0332f426d1c2d3
JinyongHa <jy200.ha@samsung.com>
2023-02-02 06:04:13 +0000

    make read-only for failed raid device

--- drivers/md/md.c
+++ drivers/md/md.c
@@ -7961,8 +7961,15 @@ void md_error(struct mddev *mddev, struct md_rdev *rdev)
 	if (!rdev || test_bit(Faulty, &rdev->flags))
 		return;
 
-	if (!mddev->pers || !mddev->pers->error_handler)
+	if (!mddev->pers || !mddev->pers->error_handler) {
+    // For RAID0
+    if (mddev->gendisk) {
+      mddev->gendisk->part0->bd_read_only = true;
+    } else {
+      printk("raid failed without gendisk");
+    }
 		return;
+  }
 	mddev->pers->error_handler(mddev, rdev);
 
 	if (mddev->degraded && !test_bit(MD_BROKEN, &mddev->flags))
@@ -7972,7 +7979,13 @@ void md_error(struct mddev *mddev, struct md_rdev *rdev)
 	if (!test_bit(MD_BROKEN, &mddev->flags)) {
 		set_bit(MD_RECOVERY_NEEDED, &mddev->recovery);
 		md_wakeup_thread(mddev->thread);
-	}
+	} else {
+    if (mddev->gendisk) {
+      mddev->gendisk->part0->bd_read_only = true;
+    } else {
+      printk("raid failed without gendisk");
+    }
+  }
 	if (mddev->event_work.func)
 		queue_work(md_misc_wq, &mddev->event_work);
 	md_new_event();
