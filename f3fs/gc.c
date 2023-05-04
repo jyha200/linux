211a73369a20937b58ca749e8c85a9246690053c
Jinyong Ha <jyha200@gmail.com>
2023-05-04 09:22:37 +0000

    add GC worker thread

--- f3fs/gc.c
+++ f3fs/gc.c
@@ -171,6 +171,20 @@ next:
 	return 0;
 }
 
+static int gc_worker_func(void* data)
+{
+  struct worker_arg* worker_arg = (struct worker_arg*)data;
+  while (!kthread_should_stop()) {
+    if (atomic_read(&worker_arg->state) == 1) {
+      worker_arg->ret = do_gc(worker_arg->sbi, worker_arg->gc_control);
+      atomic_set(&worker_arg->state, 0);
+    } else {
+      msleep(1);
+    }
+  }
+  return 0;
+}
+
 int f3fs_start_gc_thread(struct f3fs_sb_info *sbi)
 {
 	struct f3fs_gc_kthread *gc_th;
@@ -193,6 +207,16 @@ int f3fs_start_gc_thread(struct f3fs_sb_info *sbi)
 	sbi->gc_thread = gc_th;
 	init_waitqueue_head(&sbi->gc_thread->gc_wait_queue_head);
 	init_waitqueue_head(&sbi->gc_thread->fggc_wq);
+  for (int i = 0 ; i < NUM_GC_WORKER ; i++) {
+    atomic_set(&sbi->gc_thread->worker_args[i].state, 0);
+    sbi->gc_thread->worker_args[i].sbi = sbi;
+    sbi->gc_thread->worker_args[i].gc_control = NULL;
+    sbi->gc_thread->gc_workers[i] = kthread_run(
+      gc_worker_func,
+      &sbi->gc_thread->worker_args[i],
+      "gc worker %d", i);
+    printk("gc worker: %p",sbi->gc_thread->gc_workers[i]);
+  }
 	sbi->gc_thread->f3fs_gc_task = kthread_run(gc_thread_func, sbi,
 			"f3fs_gc-%u:%u", MAJOR(dev), MINOR(dev));
 	if (IS_ERR(gc_th->f3fs_gc_task)) {
@@ -210,10 +234,15 @@ void f3fs_stop_gc_thread(struct f3fs_sb_info *sbi)
 
 	if (!gc_th)
 		return;
+
+	sbi->gc_thread = NULL;
 	kthread_stop(gc_th->f3fs_gc_task);
+  for (int i = 0 ; i < NUM_GC_WORKER ; i++) {
+    kthread_stop(gc_th->gc_workers[i]);
+  }
+
 	wake_up_all(&gc_th->fggc_wq);
 	kfree(gc_th);
-	sbi->gc_thread = NULL;
 }
 
 static int select_gc_type(struct f3fs_sb_info *sbi, int gc_type)
@@ -1751,7 +1780,7 @@ skip:
 	return seg_freed;
 }
 
-int f3fs_gc(struct f3fs_sb_info *sbi, struct f3fs_gc_control *gc_control)
+int do_gc(struct f3fs_sb_info *sbi, struct f3fs_gc_control *gc_control)
 {
 	int gc_type = gc_control->init_gc_type;
 	unsigned int segno = gc_control->victim_segno;
@@ -1763,7 +1792,7 @@ int f3fs_gc(struct f3fs_sb_info *sbi, struct f3fs_gc_control *gc_control)
 		.iroot = RADIX_TREE_INIT(gc_list.iroot, GFP_NOFS),
 	};
 	unsigned int skipped_round = 0, round = 0;
-
+  
 	trace_f3fs_gc_begin(sbi->sb, gc_type, gc_control->no_bg_gc,
 				gc_control->nr_free_secs,
 				get_pages(sbi, F3FS_DIRTY_NODES),
@@ -1875,12 +1904,41 @@ stop:
 				reserved_segments(sbi),
 				prefree_segments(sbi));
 
-	f3fs_up_write(&sbi->gc_lock);
-
 	put_gc_inode(&gc_list);
 
 	if (gc_control->err_gc_skipped && !ret)
 		ret = sec_freed ? 0 : -EAGAIN;
+	return ret;
+
+}
+
+int f3fs_gc(struct f3fs_sb_info *sbi, struct f3fs_gc_control *gc_control)
+{
+  int ret = 0;
+
+  ret = do_gc(sbi, gc_control);
+  if (sbi->gc_thread) {
+    for (int i = 0 ; i < NUM_GC_WORKER ; i++) {
+      sbi->gc_thread->worker_args[i].gc_control = gc_control;
+      atomic_set(&sbi->gc_thread->worker_args[i].state, 1);
+    }
+    for (int i = 0 ; i < NUM_GC_WORKER ; i++) {
+      int local_ret;
+      while (atomic_read(&sbi->gc_thread->worker_args[i].state) == 1) {
+        msleep(1);
+      }
+      local_ret = sbi->gc_thread->worker_args[i].ret;
+      if (ret < 0) {
+        if (local_ret < 0) {
+          ret = local_ret;
+        } else {
+          ret += local_ret;
+        }
+      }
+    }
+  }
+//  ret = do_gc(sbi, gc_control);
+  f3fs_up_write(&sbi->gc_lock);
 	return ret;
 }
 
