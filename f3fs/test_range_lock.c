8f7328f729a6c01872f58595f1dd49b4690ce1b7
Jinyong Ha <jyha200@gmail.com>
2023-09-12 03:38:37 +0000

    Scalable range lock

--- f3fs/test_range_lock.c
+++ f3fs/test_range_lock.c
@@ -3,7 +3,11 @@
 #include <unistd.h>
 #include <stdbool.h>
 #include <assert.h>
+#include <pthread.h>
 
+#define VER (2)
+
+#if VER==1
 #include "range_lock.h"
 
 void* test0_thread0(void* data) {
@@ -30,11 +34,53 @@ void* test0_thread1(void* data) {
   f3fs_up_range(lock, 1, 0xFFFFFFFE, false);
   f3fs_up_range(lock, 0, 0x1, true);
 }
+#else
+#include "lockfree_list.h"
+
+void* test0_thread0(void* data) {
+  struct f3fs_rwsem3* lock = (struct f3fs_rwsem3*)data;
+  bool ret = false;
+  struct RangeLock* range = NULL;
+
+  range = RWRangeAcquire(&lock->list_rl,0, 0xFFFFFFFF, true);
+  assert(range);
+  sleep(1);
+  MutexRangeRelease(range);
+  sleep(0.5);
+  range = RWRangeTryAcquire(&lock->list_rl, 0, 1, false);
+  assert(!range);
+  range = RWRangeTryAcquire(&lock->list_rl, 1, 0xFFFFFFFF, false);
+  assert(range);
+  sleep(1);
+  MutexRangeRelease(range);
+}
+
+void* test0_thread1(void* data) {
+  struct f3fs_rwsem3* lock = (struct f3fs_rwsem3*)data;
+  struct RangeLock* ret = NULL;
+  struct RangeLock* ret2 = NULL;
+  sleep(0.5);
+  ret = RWRangeTryAcquire(&lock->list_rl, 0, 1, true);
+  assert(!ret);
+  ret = RWRangeAcquire(&lock->list_rl, 0, 1, true);
+  sleep(1);
+  ret2 = RWRangeTryAcquire(&lock->list_rl, 1, 0xFFFFFFFF, false);
+  assert(ret2);
+  MutexRangeRelease(ret2);
+  MutexRangeRelease(ret);
+}
+#endif
 
 int main(void) {
-  struct f3fs_rwsem2 lock;
   pthread_t pthread[2] = {0,};
+#if VER==1
+  struct f3fs_rwsem2 lock;
   init_f3fs_rwsem2(&lock);
+#else
+  struct f3fs_rwsem3 lock;
+  init_f3fs_rwsem3(&lock);
+#endif
+
   printf("start\n");
 
   // test0
